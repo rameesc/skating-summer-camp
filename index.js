6e4d09c6df5f9f6824be9f2df90c18872e8a6d29
ramees <ramees@gmail.com>
2026-03-25 15:15:42 +0530

    close

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 
 const popup=document.getElementsByClassName("global")[0]
+const time=document.getElementsByClassName("time")[0]
 const myElement=document.getElementById("back_btn")
+
+ 
+
 
 function autoClose(){
     
@@ -19,6 +23,45 @@ autoClose()
 myElement.addEventListener("click",()=>{
   window.history.back()
 })
+
+         let diff = Math.max(0, 1774433235583 - Date.now());
+
+    let minutes = Math.floor(diff / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);    
+function showTimerToClose(){
+
+    
+
+    let value=10
+
+    if(minutes==0 && seconds==0){
+        time.style.display="none"
+        return
+    }
+
+    const cleareTimer=setInterval(()=>{
+
+        if(minutes==0 && seconds==0) {
+            clearInterval(cleareTimer)
+            time.style.display="none"
+            return
+        }
+
+        if(seconds < 1){
+            minutes--
+            seconds=60
+        }
+
+       seconds--
+       
+         time.innerText=`Registration closes soon : ${minutes}:${seconds--}`
+
+    },1000)
+   
+
+}
+showTimerToClose()
+
 
 
 
